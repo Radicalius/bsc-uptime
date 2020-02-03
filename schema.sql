1b8ea833ba6d024162fe2da294bec77031713f3b
Radicalius <mr.zacharycotton@gmail.com>
2020-02-02 20:13:28 -0800

    Bookeeping for Ping Endpoint

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS monitors (
   name TEXT,
   key TEXT,
+  email TEXT,
   lastPing INT,
   up24h INT,
   up7d INT,
   up30d INT
 );
 
-INSERT INTO monitors VALUES ("kingman", "asdadggjasdhsadg", 0,0,0,0);
+INSERT INTO monitors VALUES ("kingman", "asdadggjasdhsadg", "mr.zacharycotton@gmail.com", 0,0,0,0);
--- srv.py
+++ srv.py
@@ -1,12 +1,17 @@
-import sqlite3, sys, time
+import sqlite3, time, sys
 import flask
 import hashlib
 from flask import Flask, request
 
 app = Flask(__name__)
 
+ping_interval = 300
+
+def click():
+    return int(time.time() // ping_interval)
+
 def hash_key(key):
-    return hashlib.sha256((key+"|"+str(int(time.time())%300)).encode("utf8")).hexdigest()
+    return hashlib.sha256((key+"|"+str(click())).encode("utf8")).hexdigest()
 
 @app.route("/ping/<monitor>", methods=["POST"])
 def ping(monitor):
@@ -16,12 +21,14 @@ def ping(monitor):
         curr.execute("SELECT key FROM monitors WHERE name = ?", [monitor])
         key = curr.fetchone()[0]
         print(key)
-        if (request.get_json()["credentials"] == hashlib):
+        if (request.get_json()["credentials"] == hash_key(key)):
+            curr.execute("UPDATE monitors SET lastPing = ?, up24h = up24h+1, up7d = up7d+1, up30d=up30d+1", [int(time.time())])
+            con.commit()
             return "", 201
         else:
             return "Unauthorized", 403
     except:
-        print (sys.exc_info())
+        print(sys.exc_info())
         return "Invalid Request", 400
 
 if __name__ == "__main__":
