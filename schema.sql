b2bc38b252b615445a9c28235bb1e4bf39ff9e9f
Radicalius <mr.zacharycotton@gmail.com>
2020-02-02 18:17:09 -0800

    SQLite BoilerPlate

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS monitors (
+  name TEXT,
+  key TEXT,
+  lastPing INT,
+  up24h INT,
+  up7d INT,
+  up30d INT
+)
--- srv.py
+++ srv.py
@@ -1,7 +1,18 @@
+import sqlite3
 import flask
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/ping/<monitor>", methods=["POST"])
+def ping(monitor):
+    print(monitor)
+    return ""
+
 if __name__ == "__main__":
+
+    con = sqlite3.connect("main.db")
+    curr = con.cursor()
+    curr.execute(open("schema.sql", "r").read())
+
     app.run(host="0.0.0.0", port="25522")
