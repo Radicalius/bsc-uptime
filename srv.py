62c455131d0985443cf0a845682d1c32a70356c1
Radicalius <mr.zacharycotton@gmail.com>
2020-02-05 18:26:41 -0800

    Set Multiple Email Targets

--- srv.py
+++ srv.py
@@ -2,11 +2,16 @@ import sqlite3, time, sys, smtplib, ssl, _thread, datetime, random, string
 import flask
 import hashlib
 from flask import Flask, request, render_template, redirect
+from email.message import EmailMessage
 
 def perc(a,b):
+    if (a + b == 0):
+        return 0
     return (a / (a+b)) * 100
 
 def big_perc(a,b):
+    if (a + b == 0):
+        return 0
     return (a / (a+b)) * 100 + (b/(a+b)) * 100
 
 app = Flask(__name__)
@@ -33,11 +38,8 @@ def index():
 def add():
     con = sqlite3.connect("main.db")
     curr = con.cursor()
-    try:
-        curr.execute('INSERT INTO monitors VALUES (?, ?, "mr.zacharycotton@gmail.com", 0,0,0,0,0,0,0, true)', [request.form["name"], request.form["key"]])
-        con.commit()
-    except:
-        print(sys.exc_info())
+    curr.execute('INSERT INTO monitors VALUES (?, ?, ?, 0,0,0,0,0,0,0,1)', [request.form["name"], request.form["key"], request.form["contacts"]])
+    con.commit()
     return redirect("/")
 
 @app.route("/client/<monitor>")
@@ -70,17 +72,17 @@ def ping(monitor):
 def send_email(receiver_email, monitor, state):
     port = 465
     smtp_server = "smtp.gmail.com"
-    sender_email = "mr.zacharycotton@gmail.com"
+    sender_email = "bscuptime@gmail.com"
     password = open("credentials").read()
-    message = """\
-    Subject: Monitor {0} is {1}
-
-    Monitor {0} is {1}.""".format(monitor, state)
-
+    message = EmailMessage()
+    message["From"] = "bscuptime@gmail.com"
+    message["To"] = receiver_email
+    message["Subject"] = "Monitor {0} is {1}".format(monitor, state)
+    message.set_content("Monitor {0} is {1}".format(monitor, state))
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.send_message(message)
 
 def mailer():
     con = sqlite3.connect("main.db")
@@ -89,13 +91,14 @@ def mailer():
         curr.execute("SELECT name, lastPing, email, state FROM monitors")
         for monitor in curr.fetchall():
             name, lastPing, email, state = monitor
-            print(time.time() - lastPing, ping_interval, state)
             if (time.time() - lastPing > ping_interval and state):
-                curr.execute("UPDATE monitors SET state = false WHERE name = ?", [name])
-                send_email(email, name, "DOWN")
+                curr.execute("UPDATE monitors SET state = 0 WHERE name = ?", [name])
+                for i in str(email).split(","):
+                    send_email(i.strip(), name, "DOWN")
             if (time.time() - lastPing <= ping_interval and not state):
-                curr.execute("UPDATE monitors SET state = true WHERE name = ?", [name])
-                send_email(email, name, "UP")
+                curr.execute("UPDATE monitors SET state = 1 WHERE name = ?", [name])
+                for i in str(email).split(","):
+                    send_email(i.strip(), name, "UP")
             if (not state):
                 curr.execute("UPDATE monitors SET down24h = down24h+1, down7d = down7d+1, down30d=down30d+1 WHERE name = ?", [name])
         con.commit()
