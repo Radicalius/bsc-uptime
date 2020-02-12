7618651735842b1bb843b3deec78ac519749e562
Radicalius <mr.zacharycotton@gmail.com>
2020-02-12 10:18:26 -0800

    Make client more robust to internet failure

--- templates/client.py
+++ templates/client.py
@@ -3,5 +3,8 @@ import requests, time, hashlib
 ping_interval = {{ping_interval}}
 
 while True:
-    requests.post("https://bscuptime.herokuapp.com/ping", json={"user": "{{user}}", "monitor": "{{monitor}}", "credentials": hashlib.sha256(("{{key}}"+"|"+str(int(time.time())//ping_interval)).encode("utf8")).hexdigest()})
+    try:
+        requests.post("https://bscuptime.herokuapp.com/ping", json={"user": "{{user}}", "monitor": "{{monitor}}", "credentials": hashlib.sha256(("{{key}}"+"|"+str(int(time.time())//ping_interval)).encode("utf8")).hexdigest()})
+    except:
+        pass
     time.sleep(ping_interval)
